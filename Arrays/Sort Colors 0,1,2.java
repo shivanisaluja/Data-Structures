3bd72a423768841f26fce01df325235ad1e00fc6
shivanisaluja <64410781+shivanisaluja@users.noreply.github.com>
2021-10-02 14:01:54 +0530

    Update Sort Colors 0,1,2.java

--- Arrays/Sort Colors 0,1,2.java
+++ Arrays/Sort Colors 0,1,2.java
@@ -1,3 +1,72 @@
+// method 1
+// time complexity O(N)
+// Space Complexity O(1)
+package ArrayList;
+
+public class Q1m1 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {1,1,2,0,0,2,1};
+		int zeroCount=0;
+		int oneCount=0;
+		int i;
+		
+//		count no. of 0's
+		for(i=0;i<arr.length;i++) {
+			if(arr[i]==0) {
+				zeroCount++;
+			}
+//			count no. of 1's
+			
+			if(arr[i]==1) {
+				oneCount++;
+				
+			}
+		}
+//		setting value 0
+		
+		for( i=0;i<zeroCount;i++) {
+			arr[i]=0;
+		}
+//		setting value 1
+		for( i=zeroCount;i<(zeroCount+oneCount);i++) {
+			arr[i]=1;
+		}
+		
+// setting value 2
+		for( i=(zeroCount+oneCount);i<arr.length;i++) {
+			arr[i]=2;
+		}
+		
+		
+//		printing the sorted array
+		for(i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+		
+		
+			
+		
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// method 2
+// time complexity O(N)
+// Space Complexity O(1)
 package ArrayList;
 
 public class Q1m2 {
